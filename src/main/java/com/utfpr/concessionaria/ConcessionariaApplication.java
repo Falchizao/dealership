337ac6e880a5e1fa7293478b8e85c29e4daab949
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-05-03 20:07:50 -0300

    Initial commit

---
+++ src/main/java/com/utfpr/concessionaria/ConcessionariaApplication.java
@@ -0,0 +1,13 @@
+package com.utfpr.concessionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utfpr/concessionaria/ConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.concessionaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
