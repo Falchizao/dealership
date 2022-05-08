651884f4245d2ede03e9242a10905d048e37aff0
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-05-08 18:34:09 -0300

    Greetings for the user

--- src/main/java/com/utfpr/concessionaria/ConcessionariaApplication.java
+++ src/main/java/com/utfpr/concessionaria/ConcessionariaApplication.java
@@ -2,11 +2,19 @@ package com.utfpr.concessionaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ConcessionariaApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConcessionariaApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void greetings() {
+        System.out.println("Obrigado por utilizar nossos serviços de concessionaria!");
+
     }
 
 }
