26982c793dc56a589fb99e8ed00c85d9146a24b9
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-05-08 18:39:12 -0300

    Enum status venda

---
+++ src/main/java/com/utfpr/concessionaria/view/entities/enums/StatusVenda.java
@@ -0,0 +1,13 @@
+package com.utfpr.concessionaria.view.entities.enums;
+
+public enum StatusVenda {
+    ABERTA,
+    FECHADA,
+    CANCELADA,
+    PENDENTE,
+    PAGAMENTO_PENDENTE,
+    PAGAMENTO_REALIZADO,
+    PAGAMENTO_CANCELADO;
+
+    StatusVenda() {}
+}
