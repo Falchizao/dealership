a06cd3ac7ff58e2aa5ca5b15fdf800bcd087e6ef
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-05-27 19:28:49 -0300

    Constants Properties added

--- src/main/java/com/utfpr/concessionaria/services/REGRAS/DescontoService.java
+++ src/main/java/com/utfpr/concessionaria/services/REGRAS/DescontoService.java
@@ -1,29 +1,57 @@
 package com.utfpr.concessionaria.services.REGRAS;
 
-import com.utfpr.concessionaria.enums.TipoDesconto;
+import com.utfpr.concessionaria.enums.PermissoesAtendente;
 import com.utfpr.concessionaria.enums.TipoVenda;
 import com.utfpr.concessionaria.modelException.error.ErrorMessage;
 import com.utfpr.concessionaria.repositores.VendaRepository;
+import com.utfpr.concessionaria.services.CRUD.AtendentesCRUDservice;
 import com.utfpr.concessionaria.view.entities.Venda;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 
 @Service
 @Slf4j
-public record DescontoService(VendaRepository vendaRepository) {
+public record DescontoService(VendaRepository vendaRepository, AtendentesCRUDservice atendentesCRUDservice){
+
+    @Value("${desconto.condicao.um}")
+    private static int conditionDiscount;
+
+    @Value("${desconto.condicao.dois}")
+    private static int conditionDiscountTwo;
+
+    @Value("${desconto.condicao.tres}")
+    private static int conditionDiscountThree;
+
+    @Value("${desconto.parcelado}")
+    private static int discountParcelado;
+
+    @Value("${desconto.debito}")
+    private static int discountDebito;
+
+    @Value("${desconto.credito}")
+    private static int disctountCredito;
+
+    @Value("${desconto.dinheiro}")
+    private static int discountDinheiro;
+
+
 
     //Aqui vão as regras de desconto da minha concessionaria em relação a descontos de venda
-
     public Venda inspecionaValorVenda(Venda venda) {
+
         log.info("Inspecionando valor da venda para atribuição de desconto");
+
         Double valorVenda = venda.getValorTotal();
-
-        if(valorVenda < 4000) return venda; //Sem desconto pra quem não tem dinheiro, vai trabalhar carai
-
-        BigDecimal valueDiscount = calculaDesconto(venda, venda.getFormaPagamento());
-        log.info("Valor do desconto: " + valueDiscount);
+        if(valorVenda < 4000){
+            log.info("Valor da venda é menor que R$ 4000,00, sem desconto!");
+            return venda;
+        }
+        if(this.tipoDescontoFuncionario(venda) != 0 && this.tipoDescontoFuncionario(venda) != 3){
+            BigDecimal valueDiscount = calculaDesconto(venda, venda.getFormaPagamento());
+            log.info("Valor do desconto: " + valueDiscount);
+        }
 
         return venda;
     }
@@ -36,17 +64,17 @@ public record DescontoService(VendaRepository vendaRepository) {
         //Desconto Padrão
         if(valueVenda == null){
             throw new ErrorMessage("Valor da venda não pode ser nulo");
-        }else if(valueVenda < 5000){ //2% de desconto
+        }else if(valueVenda < conditionDiscount){ //2% de desconto
             ret = (valueVenda * 0.02 );
             return setDesconto(venda, ret);
-        }else if(valueVenda >= 5000 && valueVenda < 10000) { //7% de desconto
+        }else if(valueVenda >= conditionDiscount && valueVenda < conditionDiscountTwo) { //7% de desconto
             ret = (valueVenda * 0.07 );
             return setDesconto(venda, ret);
-        }else if(valueVenda >= 10000) { //10% de desconto
+        }else if(valueVenda >= conditionDiscountTwo && valueVenda < conditionDiscountThree) { //10% de desconto
             ret = (valueVenda * 0.10 );
             return setDesconto(venda, ret);
         }else{
-            ret = (valueVenda * 0.15 );
+            ret = (valueVenda * 0.15 ); //15% para acima de R$ 10.000,00
             return setDesconto(venda, ret);
         }
     }
@@ -56,24 +84,23 @@ public record DescontoService(VendaRepository vendaRepository) {
 
         int discountPercentage = 0; //temp
 
-
-        //Desconto Adicional
-        if(venda.getFormaPagamento().equals(TipoVenda.DINHEIRO)){ //Mais 10% de desconto
-            discount += discount - (valueTotal * 0.10 );
-            discountPercentage = 10;
-
-        } else if(venda.getFormaPagamento().equals(TipoVenda.CARTAO_DEBITO)) { //Mais 5% de desconto
-            discount += (valueTotal * 0.05);
-            discountPercentage = 5;
-        }else if(venda.getFormaPagamento().equals(TipoVenda.CARTAO_CREDITO)) { //Mais 6% de desconto
-            discount += (valueTotal * 0.06 );
-            discountPercentage = 6;
-        }else if(venda.getFormaPagamento().equals(TipoVenda.PARCELADO)) { //Mais 2% de desconto
-            discount += (valueTotal * 0.02 );
-            discountPercentage = 2;
+        if(tipoDescontoFuncionario(venda) == 1){
+            //Desconto Adicional
+            if(venda.getFormaPagamento().equals(TipoVenda.DINHEIRO)){ //Mais 10% de desconto
+                discount += discount - (valueTotal * discountDinheiro);
+                discountPercentage = 10;
+            } else if(venda.getFormaPagamento().equals(TipoVenda.CARTAO_DEBITO)) { //Mais 5% de desconto
+                discount += (valueTotal * discountDebito);
+                discountPercentage = 5;
+            }else if(venda.getFormaPagamento().equals(TipoVenda.CARTAO_CREDITO)) { //Mais 6% de desconto
+                discount += (valueTotal * disctountCredito);
+                discountPercentage = 7;
+            }else if(venda.getFormaPagamento().equals(TipoVenda.PARCELADO)) { //Mais 2% de desconto
+                discount += (valueTotal * discountParcelado);
+                discountPercentage = 2;
+            }
+            log.info("Seu método de pagamento permite mais " + discountPercentage+ "% de desconto!");
         }
-
-        log.info("Seu método de pagamento permite mais " + discountPercentage+ "% de desconto!");
 
         venda.setDesconto(new BigDecimal(discount)); //Atribui o desconto
         venda.setValorFinal(new BigDecimal(venda.getValorTotal() - discount)); //Atribui o valor final
@@ -81,5 +108,18 @@ public record DescontoService(VendaRepository vendaRepository) {
         return venda.getDesconto();
     }
 
+    public Integer tipoDescontoFuncionario(Venda venda) {
+        PermissoesAtendente ps = atendentesCRUDservice.getPermissao(venda.getAtendente());
+        switch(ps){
+            case PERMITEDESCONTOCOMPLETO:
+                return 1;
+            case PERMITEDESCONTOPARCIAL:
+                return 2;
+            case SEMDESCONTO:
+                return 3;
+            default:
+                return 0;
+        }
+    }
 
 }
