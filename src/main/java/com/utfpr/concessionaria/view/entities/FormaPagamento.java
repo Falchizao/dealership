f0a634d37f6b5ad6447c83a932ba676d985305bc
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-05-31 00:06:17 -0300

    PaymentMethod model created

--- src/main/java/com/utfpr/concessionaria/view/entities/FormaPagamento.java
+++ src/main/java/com/utfpr/concessionaria/view/entities/FormaPagamento.java
@@ -1,16 +1,42 @@
 package com.utfpr.concessionaria.view.entities;
 
 import com.utfpr.concessionaria.enums.TipoVenda;
-import lombok.Getter;
-import lombok.Setter;
+import com.utfpr.concessionaria.generic.IModel;
+import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+
+@Entity(name = "forma_pagamento")
 @Getter
 @Setter
-public class FormaPagamento {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FormaPagamento extends IModel {
+
+    @Column(name = "tipoVenda")
     private TipoVenda tipoVenda;
+
+    @Column(name = "valor_total")
+    private Double valorTotal;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "valor")
     private Double valor;
+
+    @Column(name = "parcelas")
     private Integer parcelas;
+
+    @Column(name = "desconto")
+    private BigDecimal desconto;
+
+    @Column(name = "valor_final_desconto")
+    private BigDecimal valorFinal;
 }
 
 
