fc3fc9a1b900fa6e12aa8e11a5597172b570f92a
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-05-31 00:07:36 -0300

    Discount Routine implemented

--- src/main/java/com/utfpr/concessionaria/services/REGRAS/DescontoService.java
+++ src/main/java/com/utfpr/concessionaria/services/REGRAS/DescontoService.java
@@ -1,19 +1,26 @@
 package com.utfpr.concessionaria.services.REGRAS;
 
+import com.utfpr.concessionaria.dto.CarroDTO;
 import com.utfpr.concessionaria.enums.PermissoesAtendente;
 import com.utfpr.concessionaria.enums.TipoVenda;
 import com.utfpr.concessionaria.modelException.error.ErrorMessage;
+import com.utfpr.concessionaria.repositores.PaymentRepository;
 import com.utfpr.concessionaria.repositores.VendaRepository;
 import com.utfpr.concessionaria.services.CRUD.AtendentesCRUDservice;
+import com.utfpr.concessionaria.services.CRUD.CarrosCRUDservice;
+import com.utfpr.concessionaria.view.entities.Carro;
+import com.utfpr.concessionaria.view.entities.FormaPagamento;
 import com.utfpr.concessionaria.view.entities.Venda;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 @Slf4j
-public record DescontoService(VendaRepository vendaRepository, AtendentesCRUDservice atendentesCRUDservice){
+public record DescontoService(VendaRepository vendaRepository, AtendentesCRUDservice atendentesCRUDservice, ParcelamentoService parcelamentoService,
+                              CarrosCRUDservice carrosCRUDservice){
 
     @Value("${desconto.condicao.um}")
     private static int conditionDiscount;
@@ -30,62 +37,67 @@ public record DescontoService(VendaRepository vendaRepository, AtendentesCRUDser
     @Value("${desconto.dinheiro}")
     private static int discountDinheiro;
 
+
     //Aqui vão as regras de desconto da minha concessionaria em relação a descontos de venda
-    public Venda inspecionaValorVenda(Venda venda) {
+    public Venda inspecionaValorVenda(Venda venda, Integer typepayment) {
         log.info("Inspecionando valor da venda para atribuição de desconto");
 
-        if(this.tipoDescontoFuncionario(venda) != 0 && this.tipoDescontoFuncionario(venda) != 3){ //Casa the worker has discount permission granted
-            BigDecimal valueDiscount = setDesconto(venda);
-            log.info("Valor do desconto: " + valueDiscount);
-        }
+        Optional<CarroDTO> car = carrosCRUDservice.getById(venda.getCarro()); //Valor Compra
+
+        Long idVenda = setDesconto(car.get().getValor(), typepayment, tipoDescontoFuncionario(venda)); //Dados Computados com desconto
+        //E Parcelas
+
         return venda;
     }
 
-    public BigDecimal setDesconto(Venda venda) {
+    public Long setDesconto(Double valueCompra, Integer typepayment, Integer tipoFuncionario) {
 
-        Double discount ; //Discount
+        Double discount = 0.0; //Discount
         int discountPercentage = 0; //temp
-        Double valueVenda = venda.getValorTotal();
 
-        //Desconto Padrão
-        if(valueVenda == null){
-            throw new ErrorMessage("Valor da venda não pode ser nulo");
-        }else if(valueVenda < conditionDiscount){ //2% de desconto
-            log.info("Seu desconto é de 2%");
-            discount = (valueVenda * 0.02 );
-        }else if(valueVenda >= conditionDiscount && valueVenda < conditionDiscountTwo) { //7% de desconto
-            log.info("Seu desconto é de 7%");
-            discount = (valueVenda * 0.07 );
-        }else if(valueVenda >= conditionDiscountTwo && valueVenda < conditionDiscountThree) { //10% de desconto
-            log.info("Seu desconto é de 2%");
-            discount = (valueVenda * 0.10 );
-        }else{
-            discount = (valueVenda * 0.15 ); //15% para acima de R$ 10.000,00
-            log.info("Seu desconto é de 15%");
+        if(tipoFuncionario != 0 && tipoFuncionario != 3) { //Casa the worker has discount permission granted
+            //Desconto Padrão
+            if(valueCompra == null){
+                throw new ErrorMessage("Valor da venda não pode ser nulo");
+            }else if(valueCompra < conditionDiscount){ //2% de desconto
+                log.info("Seu desconto é de 2%");
+                discount = (valueCompra * 0.02 );
+            }else if(valueCompra >= conditionDiscount && valueCompra < conditionDiscountTwo) { //7% de desconto
+                log.info("Seu desconto é de 7%");
+                discount = (valueCompra * 0.07 );
+            }else if(valueCompra >= conditionDiscountTwo && valueCompra < conditionDiscountThree) { //10% de desconto
+                log.info("Seu desconto é de 2%");
+                discount = (valueCompra * 0.10 );
+            }else{
+                discount = (valueCompra * 0.15 ); //15% para acima de R$ 10.000,00
+                log.info("Seu desconto é de 15%");
+            }
         }
 
-        if(tipoDescontoFuncionario(venda) == 1){ //Case the worker has fulldiscount permission
+        if(tipoFuncionario == 1){ //Case the worker has fulldiscount permission
             //Desconto Adicional
-            if(venda.getFormaPagamento().equals(TipoVenda.DINHEIRO)){ //Mais 10% de desconto
-                discount += discount - (valueVenda * discountDinheiro);
+            if(typepayment.equals(TipoVenda.DINHEIRO)){ //Mais 10% de desconto
+                discount += discount - (valueCompra * discountDinheiro);
                 discountPercentage = 10;
-            } else if(venda.getFormaPagamento().equals(TipoVenda.CARTAO_DEBITO)) { //Mais 5% de desconto
-                discount += (valueVenda * discountDebito);
+            } else if(typepayment.equals(TipoVenda.CARTAO_DEBITO)) { //Mais 5% de desconto
+                discount += (valueCompra * discountDebito);
                 discountPercentage = 5;
-            }else if(venda.getFormaPagamento().equals(TipoVenda.CARTAO_CREDITO)) { //Mais 6% de desconto
-                discount += (valueVenda * disctountCredito);
+            }else if(typepayment.equals(TipoVenda.CARTAO_CREDITO)) { //Mais 6% de desconto
+                discount += (valueCompra * disctountCredito);
                 discountPercentage = 7;
-            }else if(venda.getFormaPagamento().equals(TipoVenda.PARCELADO)) { //Mais 2% de desconto
-                discount += (valueVenda * discountParcelado);
+            }else if(typepayment.equals(TipoVenda.PARCELADO)) { //Mais 2% de desconto
+                discount += (valueCompra * discountParcelado);
                 discountPercentage = 2;
             }
             log.info("Seu método de pagamento permite mais " + discountPercentage+ "% de desconto!");
         }
 
-        venda.setDesconto(new BigDecimal(discount)); //Atribui o desconto
-        venda.setValorFinal(new BigDecimal(venda.getValorTotal() - discount)); //Atribui o valor final
+        FormaPagamento fg = FormaPagamento.builder()
+                .desconto(new BigDecimal(discount))
+                .valorFinal(new BigDecimal(valueCompra - discount))
+                .build();
 
-        return venda.getDesconto();
+        return parcelamentoService.calculaParcelas(fg);
     }
 
     public Integer tipoDescontoFuncionario(Venda venda) { //Return the permission of the worker to apply discount
