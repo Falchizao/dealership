9748b39c24ed3396d8fdb4a8a2be8152b9b28807
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-05-31 00:08:15 -0300

    Feat: PaymentRepository created

---
+++ src/main/java/com/utfpr/concessionaria/repositores/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.utfpr.concessionaria.repositores;
+
+import com.utfpr.concessionaria.generic.IRepository;
+import com.utfpr.concessionaria.view.entities.FormaPagamento;
+
+public interface PaymentRepository extends IRepository<FormaPagamento> {
+
+
+
+}
