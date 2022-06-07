bc5eea9465b91f01398c7d69a72e34f3c89a1760
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-06-06 22:58:58 -0300

    Added MessageSource

--- src/main/java/com/utfpr/concessionaria/ConcessionariaApplication.java
+++ src/main/java/com/utfpr/concessionaria/ConcessionariaApplication.java
@@ -3,7 +3,10 @@ package com.utfpr.concessionaria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ConcessionariaApplication {
@@ -11,10 +14,18 @@ public class ConcessionariaApplication {
         SpringApplication.run(ConcessionariaApplication.class, args);
     }
 
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource bundle = new ReloadableResourceBundleMessageSource();
+        bundle.setBasename("classpath:messages");
+        bundle.setDefaultEncoding("UTF-8");
+        bundle.setCacheSeconds(1);
+        return bundle;
+    }
+
     @EventListener(ApplicationReadyEvent.class)
     public void greetings() {
         System.out.println("Obrigado por utilizar nossos serviços de concessionaria!");
-
     }
 
 }
