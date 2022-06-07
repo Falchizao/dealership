37eeda228091a15df63fb7f5b6025c46410c63b2
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-06-06 22:59:20 -0300

    Added Constants Class

---
+++ src/main/java/com/utfpr/concessionaria/Constants.java
@@ -0,0 +1,26 @@
+package com.utfpr.concessionaria;
+
+public class Constants {
+
+    public static final Integer PRIMEIRA_CONDICAO_DESCONTO = 5000;
+    public static final Integer SEGUNDA_CONDICAO_DESCONTO = 10000;
+    public static final Integer TERCEIRA_CONDICAO_DESCONTO = 20000;
+
+    public static final Double DESCONTO_PARCELADO = 0.02;
+    public static final Double DESCONTO_DEBITO = 0.05;
+    public static final Double DESCONTO_CREDITO = 0.07;
+    public static final Double DESCONTO_DINHEIRO = 0.10;
+
+    public static final Integer MAX_PARCELAS_PARCELADO = 12;
+    public static final Integer MAX_PARCELAS_DEBITO = 23;
+    public static final Integer MAX_PARCELAS_CREDITO = 24;
+
+    public static final Double ENTRADA_PARCELADO = 0.02;
+    public static final Double ENTRADA_DEBITO = 0.05;
+    public static final Double ENTRADA_CREDITO = 0.07;
+
+    public static final Double TAXA_JUROS_PARCELADO = 0.01;
+    public static final Double TAXA_JUROS_DEBITO = 0.02;
+    public static final Double TAXA_JUROS_CREDITO = 0.03;
+
+}
